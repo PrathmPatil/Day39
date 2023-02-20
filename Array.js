6aec9eb33bb2b1cd44e14f6c77f49c87926f6e53
PrathmPatil <prathmpatil2818@gmail.com>
2023-02-20 17:00:54 +0530

    Find the second minimum and maximum number in array

--- Array.js
+++ Array.js
@@ -1,10 +1,10 @@
  /*
-      UC1
-      Write a program in the following steps
-      a. Generates 10 Random 3 Digit number.
-      b. Store this random numbers into a array.
-      c. Then find the 2nd largest and the 2nd smallest element without sorting the array.
-      */
+         UC1
+         Write a program in the following steps
+         a. Generates 10 Random 3 Digit number.
+         b. Store this random numbers into a array.
+         c. Then find the 2nd largest and the 2nd smallest element without sorting the array.
+         */
  let arr = new Array();
  for (let i = 0; i < 10; i++) {
      let number = Math.floor(99 + Math.random() * 999)
@@ -24,3 +24,13 @@
  }
  console.log(arr)
  console.log("Second Smallest Number: " + arr[1] + " and " + " Second Largest Number: " + arr[arr.length - 2])
+
+ /*
+ UC2
+ Extend the above program to sort the array and then find the 2nd largest
+and the 2nd smallest element.
+ */
+ console.log(arr)
+ arrUC2.sort();
+ console.log(arrUC2)
+ console.log("Second Smallest Number: " + arrUC2[1] + " and " + " Second Largest Number: " + arrUC2[arr.length - 2])
